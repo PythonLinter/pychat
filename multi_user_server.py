d709e0fed1db469f75395108e7afcb01e7f59434
mkhfring <khajezade.mohamad@gmail.com>
2018-12-15 18:36:17 +0330

    Reimplement the multiserver.py

--- multi_user_server.py
+++ multi_user_server.py
@@ -4,27 +4,28 @@ import selectors
 HOST = '127.0.0.1'
 PORT = 65432
 
-selector = selectors.DefaultSelector()
-lock_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-lock_socket.bind((HOST, PORT))
-lock_socket.listen()
-print('listening on', (HOST, PORT))
-lock_socket.setblocking(False)
 
+selector = selectors.DefaultSelector()
+
+lock_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lock_socket.bind((HOST,PORT))
+lock_socket.listen()
+lock_socket.setblocking(False)
 selector.register(lock_socket, selectors.EVENT_READ, data=None)
+
 while True:
-    event = selector.select(timeout=None)
-    for key, mask in event:
+    events = selector.select(timeout=None)
+    for key, mask in events:
         if key.data is None:
             accept_wrapper(key.fileobj)
         else:
-            service_connection(key, mask)
+            serve_connection(key, mask)
 
 def accept_wrapper(socket):
     connection, address = socket.accept()
-    print('accept connection from', address)
     connection.setblocking(False)
-    data = types.SimpleNamespace(addr=address, inb=b'', outb=b'')
-    event = selectors.EVENT_READ | selectors.EVENT_WRITE
+    data = types.SimpleNameSpace(address=address, inb='', outb='')
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(connection, events, data=data)
 
-    selector.register(connection, event, data=data)
+
